8f80f4beb33e79e821fd0f36032eac1d32dd22c5
杨小伟 <2389469307@qq.com>
2020-10-11 22:27:09 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-11 22:07:43
+ * @LastEditTime: 2020-10-11 22:09:44
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /vue3-demo/src/views/Home.vue
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
