9e8568e6ea3f067d9c5eb3bc36cbedfbcc11603e
yangxiaowei <chris@bestseller.com.cn>
2020-10-27 18:22:43 +0800

    更改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,9 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <Counter />
   </div>
-  <Counter></Counter>
 </template>
 
 <script>
-<<<<<<< HEAD
-// @ is an alias to /src
-import Counter from '@/components/Counter'
-export default {
-  name: 'Home',
-  components: {Counter}
-=======
 
 import Counter from '@/components/Counter/Counter'
 export default {
@@ -21,6 +13,5 @@ export default {
   components: {
     Counter
   }
->>>>>>> master
 }
 </script>
