3d8784b423da97828bb3bc83b303dc648a6a1c5d
yangxiaowei <chris@bestseller.com.cn>
2020-10-27 16:15:23 +0800

    init 使用less,不使用eslint

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,4 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
-  <h1>This is an about page</h1>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,17 @@
-<!--
- * @Author: your name
- * @Date: 2020-10-11 22:07:43
- * @LastEditTime: 2020-10-11 22:09:44
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: /vue3-demo/src/views/Home.vue
--->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <Counter />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
+import Counter from '@/components/Counter/Counter'
 export default {
   name: 'Home',
+  components: {
+    Counter
+  }
 }
 </script>
