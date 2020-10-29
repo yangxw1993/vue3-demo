97615edd7af9c79e2f6b9df29b32c5bea8fb3100
yangxiaowei <chris@bestseller.com.cn>
2020-10-29 13:38:57 +0800

    update：使用ant design

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,49 @@
 <template>
-  <div id="nav">
-    {{routers}}
-    <!-- <router-link v-for="item in router" :key="item.name" :to="item.path">Home</router-link> | -->
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <a-layout-header>
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          <router-link to="/">首页</router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <router-link to="/plan">时间计划</router-link>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <router-link to="/about">关于我们</router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+       <router-view></router-view>
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
 <script>
-import router from './router'
 export default {
-  data(){
+  data() {
     return {
-      routers: router.routes
-    }
-  }
-  
-}
+      selectedKeys: ['1'],
+    };
+  },
+};
 </script>
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+#components-layout-demo-top .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home/Home.vue'
 
 const routes = [
   {
@@ -13,7 +13,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About/About.vue')
+  },
+  {
+    path: '/plan',
+    name: 'Plan',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Plan/')
   }
 ]
 
