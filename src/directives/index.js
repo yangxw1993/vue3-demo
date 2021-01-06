1505c8a969b56a205557d9ac5c4a6b0dbb263dbb
yangxiaowei <chris@bestseller.com.cn>
2021-01-06 15:23:49 +0800

    update: 自定义指令完成

--- src/directives/index.js
+++ src/directives/index.js
@@ -4,11 +4,20 @@ import copy from './copy/index'
 const directives = {
   copy,
 }
-
+/**
+ * 2.0和3.0的
+ * bind => beforeMount
+  inserted => mounted
+  beforeUpdate: 新的钩子，会在元素自身更新前触发
+  update => 移除！
+  componentUpdated => updated
+  beforeUnmount: 新的钩子，当元素自身被卸载前触发
+  unbind => unmounted
+ */
 export default {
   install(Vue) {
-    Object.keys(directives).forEach((key) => {
+    for(let key in directives){
       Vue.directive(key, directives[key])
-    })
+    }
   },
 }
--- src/views/Example/HighFn/Index.vue
+++ src/views/Example/HighFn/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div @click="onSay" v-copy="msg">{{msg}}</div>
-  <button v-copy="msg">{{msg}}</button>
+  <button v-copy="msg">复制</button>
 </template>
 
 <script>
