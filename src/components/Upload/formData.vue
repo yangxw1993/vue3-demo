4fd776feb729e4ed7a32ef6c870a6a3e78a2711f
yangxiaowei <chris@bestseller.com.cn>
2021-01-06 17:13:54 +0800

    update: 添加组件

---
+++ src/components/Upload/formData.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-card size="small" title="form-data上传" style="width: 300px">
+    <a-upload
+      v-model:fileList="fileList"
+      name="file"
+      :multiple="true"
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      :headers="headers"
+      @change="handleChange"
+    >
+    <a-button> <upload-outlined /> Click to Upload </a-button>
+  </a-upload>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name:'UploadForm',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/canvasCutImg/Index.vue
@@ -0,0 +1,216 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-14 16:38:45
+ * @LastEditTime: 2020-12-24 18:08:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue3-demo/src/views/Example/Upload/Index.vue
+-->
+<template>
+  <a-card size="small" title="裁切图片" style="width: 300px">
+    <div class="box">
+      <div class="btn-box">
+        <input
+          class="input"
+          type="file"
+          accept="image/*"
+          ref="file"
+          @change="changeFile"
+        />
+        <button class="btn">上传图片</button>
+        <div v-show="isShowImg">
+          <button class="btn" @click="changeSize(1)">放大</button>
+          <button class="btn" @click="changeSize(0)">缩小</button>
+          <button class="btn" @click="saveImg">保存图片</button>
+        </div>
+      </div>
+
+      <div
+        v-show="isShowImg"
+        class="img-view"
+        @touchstart="touchstart"
+        @touchmove="touchmove"
+      >
+        <canvas :width="canvasSize" :height="canvasSize" ref="canvas"></canvas>
+        <div
+          ref="mask"
+          class="mask"
+          :style="{ width: maskSize + 'px', height: maskSize + 'px' }"
+        ></div>
+      </div>
+      <img v-if="splitImg" :src="splitImg" />
+    </div>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: "CutImg",
+  data() {
+    return {
+      canvasSize: 400,
+      imageWidth: 0,
+      imageHeight: 0,
+      imgX: 0,
+      imgY: 0,
+      maskSize: 250,
+      splitImg: "",
+      // 是否显示图片
+      isShowImg: false,
+    };
+  },
+  methods: {
+    saveImg() {
+      const { myImage, maskSize } = this;
+      console.dir(this.$refs.mask);
+      const {
+        offsetLeft,
+        offsetTop,
+        clientHeight,
+        clientWidth,
+      } = this.$refs.mask;
+      if (!myImage) return;
+
+      //  获取制定区域的像素
+      let imageData = this.CTX.getImageData(
+        offsetLeft,
+        offsetTop,
+        clientWidth,
+        clientHeight
+      );
+      let canvas2 = document.createElement("canvas"),
+        canvasCtx = canvas2.getContext("2d");
+      canvas2.width = canvas2.height = maskSize;
+      // 像素点填充canvas
+      canvasCtx.putImageData(imageData, 0, 0, 0, 0, maskSize, maskSize);
+      // 生成图片 base64
+      this.splitImg = canvas2.toDataURL("image/png");
+    },
+    touchstart(e) {
+      console.dir(e);
+      const { clientX, clientY } = e.changedTouches[0];
+      this.startX = clientX;
+      this.startY = clientY;
+    },
+    touchmove(e) {
+      const { clientX, clientY } = e.changedTouches[0];
+      const { startX, startY } = this;
+      const changeX = clientX - startX;
+      const changeY = clientY - startY;
+      if (Math.ceil(changeX) > 10 || Math.ceil(changeY) > 10) {
+        this.imgX += changeX;
+        this.imgY += changeY;
+        this.drawImage();
+        this.startX = clientX;
+        this.startY = clientY;
+      }
+    },
+    changeFile() {
+      let file = this.$refs.file.files[0];
+      console.dir(this.$refs.file);
+      if (!file) return;
+      this.isShowImg = true;
+      // FileReader
+      let fileExample = new FileReader();
+      fileExample.readAsDataURL(file);
+      fileExample.onload = (e) => {
+        console.dir(e);
+        // 创建新图片
+        this.myImage = new Image();
+        this.myImage.src = e.target.result;
+        const { width, height } = this.myImage;
+        console.log(width, "**", height);
+        this.myImage.onload = (_) => {
+          // image's width and height
+          const { width, height } = this.myImage;
+          // 安装图片比例计算宽gao
+          let scale = 1;
+          const { canvasSize } = this;
+          if (width > height) {
+            scale = width / canvasSize;
+            this.imageWidth = canvasSize;
+            this.imageHeight = height / scale;
+          } else {
+            scale = height / canvasSize;
+            this.imageHeight = canvasSize;
+            this.imageWidth = width / scale;
+          }
+          console.log(this.imageWidth, "**", this.imageHeight, "**", scale);
+
+          // this.imageWidth = width;
+          // this.imageHeight = height
+          this.drawImage();
+        };
+      };
+    },
+    changeSize(flag) {
+      let { imageWidth, imageHeight } = this;
+      const scale = imageWidth / imageHeight,
+        scaleW = 20,
+        scaleH = scaleW / scale;
+      console.log(this.imageWidth, this.imageHeight);
+
+      if (flag) {
+        this.imageWidth += scaleW;
+        this.imageHeight += scaleH;
+      } else {
+        this.imageWidth -= scaleW;
+        this.imageHeight -= scaleH;
+      }
+      console.log(this.imageWidth, this.imageHeight);
+      this.drawImage();
+    },
+    drawImage() {
+      this.CTX = this.$refs.canvas.getContext("2d");
+      let { CTX, myImage, imageWidth, imageHeight, canvasSize } = this;
+      CTX.clearRect(0, 0, canvasSize, canvasSize);
+      // console.log(imageWidth, imageHeight );
+      CTX.drawImage(myImage, 0, 0, imageWidth, imageHeight);
+    },
+    // 上传大文件
+    changeMaxFile() {
+      let file = this.$refs.maxFile.files[0];
+      console.log(file);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.btn-box {
+  position: relative;
+  .btn {
+    position: relative;
+    width: 100px;
+    height: 40px;
+  }
+  .input {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100px;
+    height: 40px;
+    z-index: 2;
+    opacity: 0;
+  }
+}
+.img-view {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  .mask {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    border: solid 1px #ccc;
+  }
+  canvas {
+    width: 400px;
+    height: 400px;
+    border: solid 1px #ccc;
+  }
+}
+</style>
