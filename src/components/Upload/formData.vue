bd26cba040dfb508c17adcaf1d019e6013f99152
yangxiaowei <chris@bestseller.com.cn>
2021-01-08 17:02:54 +0800

    update: 上传图片

--- src/components/Upload/formData.vue
+++ src/components/Upload/formData.vue
@@ -1,26 +1,58 @@
 <template>
   <a-card size="small" title="form-data上传" style="width: 300px">
-    <a-upload
-      v-model:fileList="fileList"
-      name="file"
-      :multiple="true"
-      :headers="headers"
-      @change="handleChange"
+    <a-upload :fileList="fileList" :remove="handleRemove" :beforeUpload="beforeUpload">
+      <a-button> Select File </a-button>
+    </a-upload>
+    <a-button
+      type="primary"
+      :disabled="fileList.length === 0"
+      :loading="uploading"
+      style="margin-top: 16px"
+      @click="handleUpload"
     >
-    <a-button> <upload-outlined /> Click to Upload </a-button>
-  </a-upload>
+      {{ uploading ? 'Uploading' : 'Start Upload' }}
+    </a-button>
   </a-card>
 </template>
 
 <script>
+import {reactive, toRefs} from 'vue'
+import httpRequest from '@/utils/request'
 export default {
   name:'UploadForm',
   setup(){
-    const handleChange = (file) => {
-      console.log(file)
+    const status = reactive({
+      fileList: [],
+      uploading: false,
+    })
+     const beforeUpload = (file) => {
+      status.fileList = [...status.fileList, file];
+      return false;
+    };
+    const handleUpload = (file) => {
+      const fileList = status.fileList;
+      const formData = new FormData();
+      fileList.forEach(file => {
+        formData.append('files[]', file);
+      });
+      status.uploading = true;
+      httpRequest({
+        url: 'http://localhost:3001/upload/formData',
+        method: 'post',
+         data: formData,
+         headers: {
+           'Content-Type':'multipart/form-data'
+         }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => console.error(err))
+      // You can use any AJAX library you like
+      
     }
     return{
-      handleChange
+      ...toRefs(status),
+      handleUpload,
+      beforeUpload
     }
   }
 }
