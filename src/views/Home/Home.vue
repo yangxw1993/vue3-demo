eaa8ddb4d003cba3d10f8740468994d17109fa48
yangxiaowei <chris@bestseller.com.cn>
2021-02-04 18:58:01 +0800

    Vue3 ref获取元素

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="home">
     <Counter />
+    <div class="div-box" @mousedown="mousedown" ref="root">
+      
+    </div>
   </div>
 </template>
 
 <script>
-
+import {ref, nextTick, onMounted} from 'vue';
 import Counter from '@/components/Counter/Counter'
 export default {
   name: 'Home',
   components: {
     Counter
+  },
+  setup(){
+    const root = ref(null)
+
+    onMounted(() => {
+      // DOM元素将在初始渲染后分配给ref
+      console.log(root.value) // <div>这是根元素</div>
+    })
+    const mousedown = (e) => {
+      console.log(e);
+    };
+
+    return {
+      root,
+      mousedown
+    }
   }
 }
 </script>
+<style lang="less" scoped>
+  .div-box{
+    width: 100%;
+    height: 300px;
+    border: solid 1px red;
+  }
+</style>
